d2e20ff0f565f861f6e4efda4e8ed10464fc4ede
Eduardo Queiroz <eduardobq3@gmail.com>
2020-06-20 11:15:16 -0300

    Produto e associacao Many to Many

---
+++ src/main/java/com/eduardoqueiroz/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.eduardoqueiroz.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduardoqueiroz.cursomc.domain.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+
+}
